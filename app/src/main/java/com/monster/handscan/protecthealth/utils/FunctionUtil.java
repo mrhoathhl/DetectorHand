87295f7e49d0b5707edadaa013e16dfe0928ab4b
hoalx97 <hoalx@monsterstudio.com>
2022-04-28 22:51:31 +0700

    add policy

---
+++ app/src/main/java/com/monster/handscan/protecthealth/utils/FunctionUtil.java
@@ -0,0 +1,14 @@
+package com.monster.handscan.protecthealth.utils;
+
+import android.content.Context;
+import android.widget.FrameLayout;
+
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.nativeAds.MaxNativeAdListener;
+import com.applovin.mediation.nativeAds.MaxNativeAdLoader;
+import com.applovin.mediation.nativeAds.MaxNativeAdView;
+import com.applovin.mediation.nativeAds.MaxNativeAdViewBinder;
+import com.monster.handscan.protecthealth.R;
+
+public class FunctionUtil {
+}
